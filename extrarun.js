4c3539ee5628e6165c25a9dcb00a4ef29c54fe38
Lokesh Pal <lokeshpal9756@gmail.com>
2021-06-14 15:24:32 +0530

    third funtion added

---
+++ extrarun.js
@@ -0,0 +1,25 @@
+module.exports=function extrarun(str)
+{
+  var arr=["Sunrisers Hyderabad","Rising Pune Supergiant","Kolkata Knight Riders","Kings XI Punjab","Royal Challengers Bangalore","Mumbai Indians","Delhi Daredevils","Gujarat Lions","Chennai Super Kings","Deccan Chargers","Rajasthan Royals"]
+var ct=[];
+let sum;
+console.log("Extra run taken by per team in 2016:");
+for(let j=0;j<arr.length;j++)
+{
+  sum=0;
+for(let i=21;i<str.length-1;i++)
+{
+  if(str[i][0]>=577&&str[i][0]<=634)
+  {
+if(arr[j]==str[i][2])
+sum+=parseInt(str[i][16]);
+}
+ct[j]=sum;
+
+}
+}
+for(j=0;j<arr.length;j++)
+{
+  console.log(arr[j]+ " takes "+ct[j]+" extra runs");
+}
+}
